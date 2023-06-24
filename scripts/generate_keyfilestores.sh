13d044e3759f222cd5fe32cc650ddf41cbb2f2d2
rslijp <renzo.slijp@softcause.nl>
2023-06-24 18:14:06 +0200

    Smallfix

--- scripts/generate_keyfilestores.sh
+++ scripts/generate_keyfilestores.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 keytool -keypass changeit -storepass changeit -genkey -alias server -dname "CN=my-logging-server" -keyalg RSA -validity 365 -keystore server.keystore
-
+keytool -export -keystore server.keystore -storepass  changeit  -alias server -file server.crt
 keytool -import -alias server -file server.crt -keystore server.truststore
 
 keytool -keypass changeit -storepass changeit -import -alias server -file server.crt -keystore server.truststore
