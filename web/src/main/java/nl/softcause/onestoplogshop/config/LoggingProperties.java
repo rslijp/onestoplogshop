4029d9b6826a8b1323dbdcdb59a52ab5670b1f06
rslijp <renzo.slijp@softcause.nl>
2023-08-24 18:10:53 +0200

    Optional resource handling

--- web/src/main/java/nl/softcause/onestoplogshop/config/LoggingProperties.java
+++ web/src/main/java/nl/softcause/onestoplogshop/config/LoggingProperties.java
@@ -8,6 +8,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration(proxyBeanMethods = false)
 @ConfigurationProperties(prefix = "onestoplogshop")
+@PropertySource(ignoreResourceNotFound = true, value = "file:logging.yml", factory = YamlPropertySourceFactory.class)
 @PropertySource(value = "classpath:logging.yml", factory = YamlPropertySourceFactory.class)
 public class LoggingProperties {
 
--- web/src/main/java/nl/softcause/onestoplogshop/config/YamlPropertySourceFactory.java
+++ web/src/main/java/nl/softcause/onestoplogshop/config/YamlPropertySourceFactory.java
@@ -1,6 +1,11 @@
 package nl.softcause.onestoplogshop.config;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Instant;
+import nl.softcause.onestoplogshop.api.DiscoverLogController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
@@ -8,15 +13,31 @@ import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
 
 public class YamlPropertySourceFactory implements PropertySourceFactory {
-
+    private static final Logger logger = LoggerFactory.getLogger(YamlPropertySourceFactory.class);
 
     @Override
     public PropertySource<?> createPropertySource(String name, EncodedResource resource) throws IOException {
-        var factory = new YamlPropertiesFactoryBean();
-        factory.setResources(resource.getResource());
-
-        var properties = factory.getObject();
-
-        return new PropertiesPropertySource(resource.getResource().getFilename(), properties);
+        try {
+            final YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+            factory.setResources(resource.getResource());
+            factory.afterPropertiesSet();
+            var properties = factory.getObject();
+            return new PropertiesPropertySource(resource.getResource().getFilename(), properties);
+        } catch (final java.lang.IllegalStateException illegalState) {
+            if (illegalState.getCause() instanceof final FileNotFoundException e) {
+                logger.info("Optional property source {} not found. Using embedded resource.", resource.getResource().getFilename());
+                // ConfigurationClassParser wants FileNotFoundException to honor PropertySource.ignoreResourceNotFound
+                throw e;
+            } else {
+                throw illegalState;
+            }
+        }
+//
+//        var factory = new YamlPropertiesFactoryBean();
+//        factory.setResources(resource.getResource());
+//
+//        var properties = factory.getObject();
+//
+//        return new PropertiesPropertySource(resource.getResource().getFilename(), properties);
     }
 }
